7bd6110cac04f9e9db2344d6551ff6662aa41faa
ShreyanshShrivastava <72603193+ShreyanshShrivastava@users.noreply.github.com>
2021-05-06 22:57:04 +0530

    Add files via upload

---
+++ Setup-arch.sh
@@ -0,0 +1,7 @@
+sudo pacman -Syu
+sudo pacman -Sy samba mariadb gcc g++ libreoffice-fresh jdk-openjdk jre-openjdk a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv opus wavpack x264 xvidcore gimp android-file-transfer obs-studio virtualbox openshot gnome-chess gnome-weather steam blender wine kdenlive neofetch screenfetch 
+sudo pamac build onedrive-abraunegg
+flatpak install org.chromium.Chromium
+flatpak install com.spotify.Client
+flatpak install org.signal.Signal
+flatpak install com.google.AndroidStudio
