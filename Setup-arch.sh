81364c9b2520a5d8311be9c5d7e75fb12736bfaa
ShreyanshShrivastava <72603193+ShreyanshShrivastava@users.noreply.github.com>
2021-05-07 12:00:23 +0530

    Update Setup-arch.sh

--- Setup-arch.sh
+++ Setup-arch.sh
@@ -1,7 +1,8 @@
 sudo pacman -Syu
-sudo pacman -Sy samba mariadb gcc libreoffice-fresh jdk-openjdk jre-openjdk a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv opus wavpack x264 xvidcore gimp android-file-transfer obs-studio virtualbox openshot gnome-chess gnome-weather telegram-desktop steam blender wine kdenlive neofetch screenfetch 
+sudo pacman -Sy samba mariadb gcc libreoffice-fresh jdk-openjdk jre-openjdk a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv opus wavpack x264 xvidcore gimp android-file-transfer obs-studio virtualbox openshot gnome-chess gnome-weather telegram-desktop steam blender wine kdenlive neofetch screenfetch nautilus-share virt-manager qemu vde2 ebtables dnsmasq bridge-utils openbsd-netcat edk2-ovmf 
 sudo pamac build onedrive-abraunegg
 flatpak install org.chromium.Chromium
 flatpak install com.spotify.Client
 flatpak install org.signal.Signal
 flatpak install com.google.AndroidStudio
+sudo systemctl enable --now libvirtd
