dc931286c311fe77dd36f854f644ce71fd1a1f5a
ShreyanshShrivastava <72603193+ShreyanshShrivastava@users.noreply.github.com>
2021-05-07 11:19:17 +0530

    Update Setup-arch.sh

--- Setup-arch.sh
+++ Setup-arch.sh
@@ -1,5 +1,5 @@
 sudo pacman -Syu
-sudo pacman -Sy samba mariadb gcc g++ libreoffice-fresh jdk-openjdk jre-openjdk a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv opus wavpack x264 xvidcore gimp android-file-transfer obs-studio virtualbox openshot gnome-chess gnome-weather telegram-desktop steam blender wine kdenlive neofetch screenfetch 
+sudo pacman -Sy samba mariadb gcc libreoffice-fresh jdk-openjdk jre-openjdk a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv opus wavpack x264 xvidcore gimp android-file-transfer obs-studio virtualbox openshot gnome-chess gnome-weather telegram-desktop steam blender wine kdenlive neofetch screenfetch 
 sudo pamac build onedrive-abraunegg
 flatpak install org.chromium.Chromium
 flatpak install com.spotify.Client
