e95dd5eac801ccedf4f686a46cb1bdbe92f78b72
ShreyanshShrivastava <shreyanshshrivastavatop004@gmail.com>
2021-05-27 18:58:17 +0530

    Update After_install.sh Add additional.sh

--- After_install.sh
+++ After_install.sh
@@ -49,55 +49,10 @@ sudo sed -i "5s/1/0.25/" /etc/default/zramd
 sudo systemctl enable --now zramd
 yay -S anydesk google-chrome webapp-manager snapd
 sudo systemctl enable --now snapd
-sudo snap install g-assist --candidate
-sudo snap install video-downloader
 pamac build onedrive-abraunegg
 #Installing zsh shell with powerlevel10k
 sudo pacman -S zsh
 sudo chsh -s /usr/bin/zsh
 #Installing ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#Installing zsh theme powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sudo sed -i "18s:robbyrussell:powerlevel10k/powerlevel10k:" /home/shreyansh/.zshrc
-#Adding autosuggestion and syntax highlighting plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sudo sed -i "80s/)/ /" .zshrc
-sudo sed -i "81 i \ \ zsh-autosuggestions" /home/shreyansh/.zshrc
-sudo sed -i "82 i \ \ zsh-syntax-highlighting" .zshrc
-sudo sed -i "83 i )" .zshrc
-#Installing gnome shell extensions
-#DashToDock
-git clone https://github.com/ewlsh/dash-to-dock/
-cd dash-to-dock
-git checkout ewlsh/gnome-40
-make
-make install
-cd
-#PopShell
-sudo npm install typescript -g
-git clone https://github.com/pop-os/shell.git
-cd shell
-make local-install
-cd
-#KDE Connect
-wget https://github.com/GSConnect/gnome-shell-extension-gsconnect/releases/download/v46/gsconnect@andyholmes.github.io.zipunzip gsconnect@andyholmes.github.io.zip \ -d /home/shreyansh/.local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io.zip
-gnome-extensions install --force gsconnect@andyholmes.github.io.zip
-#Alphabateical App Grid
-git clone https://github.com/stuarthayhurst/alphabetical-grid-extension.git
-cd alphabetical-grid-extension
-./scripts/create-release.sh -i
-cd
-#Appindicator support
-git clone https://github.com/ubuntu/gnome-shell-extension-appindicator.git
-ln -s $PWD ~/.local/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com
-gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
-#OpenWeather
-yay -S gnome-shell-extension-openweather-git
-gnome-extensions enable openweather-extension@jenslody.de
-#Restarting gnome shell
-busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart("Restarting…")'
-#Configuring OneDrive
-onedrive
 sudo reboot
---
+++ additional.sh
@@ -0,0 +1,43 @@
+sudo snap install g-assist --candidate
+sudo snap install video-downloader
+#Installing zsh theme powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sudo sed -i "18s:robbyrussell:powerlevel10k/powerlevel10k:" /home/shreyansh/.zshrc
+#Adding autosuggestion and syntax highlighting plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sudo sed -i "80s/)/ /" .zshrc
+sudo sed -i "81 i \ \ zsh-autosuggestions" /home/shreyansh/.zshrc
+sudo sed -i "82 i \ \ zsh-syntax-highlighting" .zshrc
+sudo sed -i "83 i )" .zshrc
+#Installing gnome shell extensions
+#DashToDock
+git clone https://github.com/ewlsh/dash-to-dock/
+cd dash-to-dock
+git checkout ewlsh/gnome-40
+make
+make install
+cd
+#PopShell
+sudo npm install typescript -g
+git clone https://github.com/pop-os/shell.git
+cd shell
+make local-install
+cd
+#KDE Connect
+wget https://github.com/GSConnect/gnome-shell-extension-gsconnect/releases/download/v46/gsconnect@andyholmes.github.io.zipunzip gsconnect@andyholmes.github.io.zip \ -d /home/shreyansh/.local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io.zip
+gnome-extensions install --force gsconnect@andyholmes.github.io.zip
+#Alphabateical App Grid
+git clone https://github.com/stuarthayhurst/alphabetical-grid-extension.git
+cd alphabetical-grid-extension
+./scripts/create-release.sh -i
+cd
+#Appindicator support
+git clone https://github.com/ubuntu/gnome-shell-extension-appindicator.git
+ln -s $PWD ~/.local/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com
+gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
+#OpenWeather
+yay -S gnome-shell-extension-openweather-git
+gnome-extensions enable openweather-extension@jenslody.de
+onedrive
+onedrive --synchronize
