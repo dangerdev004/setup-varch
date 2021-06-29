3efa104880bf698b204e2b5ae552b912e119c84d
Shreyansh Shrivastava <shreyansh@arch.localdomain>
2021-06-29 22:20:49 +0530

    Replace gnome.sh with script.sh

--- script1.sh
+++ script1.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+r='\033[0;31m'
+g='\033[0;32m'
+n='\033[0m'
+b='\033[1m'
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
 sed -i '177s/.//' /etc/locale.gen
@@ -12,17 +15,17 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:passwd | chpasswd
 
-# You can add xorg to the installation packages, I usually add it at the DE or WM install script
-# You can remove the tlp package if you are installing on a desktop or vm
+echo -e ${g}${b}You can add xorg to the installation packages, I usually add it at the DE or WM install script${n}
+echo -e ${g}${b}You can remove the tlp package if you are installing on a desktop or vm${n}
 
 pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
-#Installing grub
+echo -e ${g}${b}Installing grub${n}
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
-#Enabling services
+echo -e ${g}${b}Enabling services${n}
 systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable cups.service
@@ -34,9 +37,9 @@ systemctl enable fstrim.timer
 systemctl enable libvirtd
 systemctl enable firewalld
 systemctl enable acpid
-#Making shreyansh as root and wheel users
-useradd -m shreyansh
-echo shreyansh:passwd | chpasswd
+echo -e ${g}${b}Making Username as root and wheel users${n}
+useradd -m Username
+echo Username:passwd | chpasswd
 groupadd libvirt
 usermod -aG libvirt shreyansh
 usermod -aG root shreyansh
@@ -45,13 +48,14 @@ echo "shreyansh ALL=(ALL) ALL" >> /etc/sudoers.d/shreyansh
 sed -i "82s/.//" /etc/sudoers
 sed -i "82s/.//" /etc/sudoers
 sed -i "82s/.//" /etc/sudoers
-#Changing timezone to IST
-timedatectl set-timezone Asia/Kolkata
+echo -e ${g}${b}Changing timezone to Timezone${n}
+timedatectl set-timezone Timezone
 timedatectl set-ntp true
 hwclock --systohc
-#Enabling the fastest and most updated mirror
+echo -e ${g}${b}Enabling the fastest and most updated mirror${n}
 reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 #Configuring initram for btrfs filesystem
+#comment all these lines if you aren't using btrfs
 sed -i '7s/./&btrfs/9' /etc/mkinitcpio.conf
 sed -i '7s/./& i915/14' /etc/mkinitcpio.conf
 sed -i '14s:.:&"/usr/bin/btrfs":10' /etc/mkinitcpio.conf
