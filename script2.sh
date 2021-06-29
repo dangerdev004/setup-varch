607f02a1071dc3aef78844bb991339afdc61ca36
dangerdev004 <72603193+dangerdev004@users.noreply.github.com>
2021-06-29 23:24:08 +0530

    Revert "Update script2.sh"

--- script2.sh
+++ script2.sh
@@ -9,8 +9,8 @@ sudo sed -i "94s/.//" /etc/pacman.conf
 sudo pacman -Syy
 #Doing system upgrade
 sudo pacman -Syu
-#Installing base packages for kde
-sudo pacman -S xorg sddm plasma kde-applications firefox simplescreenrecorder obs-studio vlc papirus-icon-theme kdenlive materia-kde dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji awesome-terminal-fonts
+#Installing base packages for gnome
+sudo pacman -S gdm gnome firefox gnome-tweaks simplescreenrecorder arc-gtk-theme arc-icon-theme obs-studio vlc dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji awesome-terminal-fonts
 #Installing additional packages for user shreyansh
 sudo pacman -Sy samba mariadb gcc krita libreoffice-fresh jdk-openjdk jre-openjdk a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv opus wavpack x264 xvidcore gimp android-file-transfer obs-studio virtualbox openshot gnome-chess gnuchess gnome-weather telegram-desktop steam blender wine wine-mono wine-gecko kdenlive neofetch screenfetch nautilus-share virt-manager qemu vde2 ebtables dnsmasq bridge-utils openbsd-netcat edk2-ovmf npm atom lollypop rhythmbox sassc
 sudo systemctl enable --now libvirtd
@@ -20,9 +20,9 @@ sudo cp smb.conf/smb.conf /etc/samba
 sudo mkdir /var/lib/samba/usershares
 sudo chown root:sambashare /var/lib/samba/usershares
 sudo chmod 1770 /var/lib/samba/usershares
-sudo smbpasswd -a Username
+sudo smbpasswd -a shreyansh
 sudo groupadd -r sambashare
-sudo gpasswd sambashare -a Username
+sudo gpasswd sambashare -a shreyansh
 sudo firewall-cmd --zone=home --add-service={samba,samba-client,samba-dc} --permanent
 sudo firewall-cmd --reload
 systemctl enable --now smb nmb
@@ -31,7 +31,7 @@ sudo chattr +C /var/lib/mysql
 sudo mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 sudo systemctl enable --now mariadb
 #Enabling gdm service on startup
-sudo systemctl enable sddm
+sudo systemctl enable gdm
 #Installing AUR helper (yay)
 git clone https://aur.archlinux.org/yay.git
 cd yay
